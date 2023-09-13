33c52478759b74c234c6f001d81abff13651bb8f
Eva Maciejko <emaciejko@gmail.com>
2023-09-13 10:45:34 -0400

    Added store component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app',
-  template: `<div class="bg-success p-2 text-center text-white">
-             Voici Boutique Sport
-            </div>`
+  template: "<app-store></app-store>"
 })
 export class AppComponent {
   title = 'boutique-sport';
