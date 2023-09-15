abbe59aab31ac620824172c281c6e8170d69aaeb
Eva Maciejko <emaciejko@gmail.com>
2023-09-15 14:43:09 -0400

    Add authentication

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,15 +4,17 @@ import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
 import { AuthComponent } from "./auth/auth.component";
 import { AdminComponent } from "./admin/admin.component";
+import { AuthGuard } from "./auth.gard";
 
 let routing = RouterModule.forChild([
     { path: "auth", component: AuthComponent },
-    { path: "main", component: AdminComponent },
+    { path: "main", component: AdminComponent, canActivate: [AuthGuard] },
     { path: "**", redirectTo: "auth" }
 ]);
 
 @NgModule({
     imports: [CommonModule, FormsModule, routing],
-    declarations: [AuthComponent, AdminComponent]
+    declarations: [AuthComponent, AdminComponent],
+    providers: [AuthGuard]
 })
 export class AdminModule { }
