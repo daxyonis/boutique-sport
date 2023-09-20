8cc3cf4e091ac417ff54f97867359c1992efee43
Eva Maciejko <emaciejko@gmail.com>
2023-09-20 14:52:07 -0400

    Added the product table with create and edit.

--- src/app/admin/product-table/product-table.component.ts
+++ src/app/admin/product-table/product-table.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, IterableDiffer, IterableDiffers, ViewChild } from "@angular/core";
+import { MatTableDataSource } from "@angular/material/table";
+import { Product } from "src/app/model/product.model";
+import { ProductRepository } from "src/app/model/product.repository";
+import { MatPaginator } from "@angular/material/paginator";
 
 @Component({
   selector: 'app-product-table',
@@ -7,4 +11,33 @@ import { Component } from '@angular/core';
 })
 export class ProductTableComponent {
 
+  @ViewChild(MatPaginator)
+  paginator? : MatPaginator
+
+  colsAndRows: string[] = ['id', 'name', 'category', 'price', 'buttons'];
+    dataSource =
+        new MatTableDataSource<Product>(this.repository.products());
+    differ: IterableDiffer<Product>;
+
+    constructor(private repository: ProductRepository,
+            differs: IterableDiffers) {
+        this.differ = differs.find(this.repository.products()).create();
+    }
+
+    ngDoCheck() {
+        let changes = this.differ?.diff(this.repository.products());
+        if (changes != null) {
+            this.dataSource.data = this.repository.products();
+        }
+    }
+
+    ngAfterViewInit() {
+      if (this.paginator) {
+          this.dataSource.paginator = this.paginator;
+      }
+    }
+
+    deleteProduct(id: number) {
+        this.repository.deleteProduct(id);
+    }
 }
